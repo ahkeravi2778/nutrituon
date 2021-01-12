7ba1f034113edf3dceea026df0ecb3770ac21254
Ravi <ravi.exe.doc@gmail.com>
2021-01-12 18:22:05 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React , { useEffect, useState } from "react";
+import React , { useEffect } from "react";
 import './App.css';
 import Header from './components/header';
 import Cards from './components/cards';
